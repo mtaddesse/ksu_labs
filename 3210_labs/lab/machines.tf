3a4ad4db2522ddd38db98b2ac82ecc7e057e1c9f
Nick Kammerdiener <nicolas@kammerdiener.us>
2023-06-12 00:34:36 -0400

    updating egress

--- 3210_labs/lab/machines.tf
+++ 3210_labs/lab/machines.tf
@@ -23,6 +23,12 @@ resource "aws_security_group" "allow_http_https_ssh" {
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "ubuntu_instance" {
