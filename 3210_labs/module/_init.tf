0d1cd9fd1f0d903675aab58d666ae46fa2165f7d
mtaddesse <143301975+mtaddesse@users.noreply.github.com>
2023-09-01 17:57:02 -0400

    Update _init.tf

--- 3210_labs/module/_init.tf
+++ 3210_labs/module/_init.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  access_key = AKIAUCTIJDCFUYSJT2F7
+  secret_key = 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
 }
